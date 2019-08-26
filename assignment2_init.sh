4b73026bf3b4bae522286719976368234e241fe0
Lucas Pierezan Magalhães <lucas.pierezan@gmail.com>
2019-08-26 17:09:40 -0300

    assignment2 init.sh

--- assignment2_init.sh
+++ assignment2_init.sh
@@ -2,10 +2,14 @@
 # git clone https://github.com/lpierezan/rl_cs234.git
 # cd rl_cs234
 # chmod +x *.sh
-# /opt/deeplearning/binaries/tensorflow/
+# /opt/deeplearning/binaries/tensorflow/tensorflow_gpu-1.14.0-cp35-cp35m-linux_x86_64.whl
 sudo apt-get -y install ffmpeg
 cd assignment2
 virtualenv .env --python=python3
 source .env/bin/activate
+pip install /opt/deeplearning/binaries/tensorflow/tensorflow_gpu-1.14.0-cp35-cp35m-linux_x86_64.whl
 pip install -r requirements.txt
 pip install gym[atari]
+
+# ready to go
+rm -r results/q5_train_atari_nature/
